c71f52570ae70070506ea59ffbb6af237e1b4625
Adam Klingbaum <adam.klingbaum@gmail.com>
2022-06-23 10:37:23 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,16 +3,34 @@
 set -e
 
 # Install oh-my-zsh
-echo "(1/4) λ Installing oh-my-zsh"
+
+echo "λ Installing oh-my-zsh"
+
 if [ -d ~/.oh-my-zsh ]; then
   echo "Found ~/.oh-my-zsh - skipping this step"
 else
   sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 fi
-echo "(1/4) ✅ Installed oh-my-zsh."
+
+echo "✅ Installed oh-my-zsh."
+
+# Install homebrew
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  echo "λ Configuring homebrew"
+  if ! which brew > /dev/null; then
+    echo ">> Installing homebrew"
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  fi;
+
+  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+  echo "✅ Configured homebrew."
+fi
 
 # Clear local vim config
-echo "(2/4) λ Clearing local vim config"
+
+echo "λ Clearing local vim config"
 if [ -d ~/.vim ]; then
   rm -rf ~/.vim
 fi
@@ -21,37 +39,67 @@ if [ -f ~/.vimrc ]; then
   rm -f ~/.vimrc
 fi
 
-echo "(2/4) ✅ Cleared local vim config."
+echo "✅ Cleared local vim config."
 
-# Install ripgrep
-echo "(3/4) λ Installing ripgrep"
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  if ! which brew > /dev/null; then
-    echo ">> Installing homebrew"
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  fi;
+# Link dotfilees
 
-  echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zprofile
-  eval "$(/opt/homebrew/bin/brew shellenv)"
+echo "λ Linking dotfiles"
 
-  echo ">> Installing via brew"
-  brew install ripgrep
-else
-  echo '>> Installing via apt-get'
-  sudo apt-get install -y ripgrep
-fi
-
-echo "(3/4) ✅ Insatlled ripgrep."
-
-# Link dotfiles
-echo "(4/4) λ Linking dotfiles"
 dotfiles=(.zshrc .vimrc .gitignore_global .gitconfig)
+
 for file in "${dotfiles[@]}"
 do
   echo "λ Linking ${file}"
   rm -f ~/$file
   ln -s ~/dotfiles/config/$file ~/$file
 done
-echo "(4/4) ✅ Linked dotfiles."
+
+echo "✅ Linked dotfiles."
+
+# Install ripgrep
+
+echo "λ Installing ripgrep"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  echo '>> Installing via homebrew'
+  brew install ripgrep
+else
+  echo '>> Installing via apt-get'
+  sudo apt-get install -y ripgrep
+fi
+
+echo "✅ Installed ripgrep."
+
+# Install plug
+
+echo "λ Installing plug"
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+
+echo "✅ Installed plug."
+
+# Install neovim
+
+echo "λ Installing neovim"
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  echo '>> Installing via homebrew'
+  brew install neovim
+else
+  echo '>> Installing via apt-get'
+  sudo apt-get install neovim
+fi
+
+echo "✅ Installed neovim."
+
+# Link neovim config
+
+echo "λ Linking neovim config"
+
+mkdir -p ~/.config/nvim
+ln -sf ~/dotfiles/.config/nvim/init.vim ~/.config/nvim/init.vim
+
+echo "✅ Linked neovim config."
 
 echo "✅ Installation complete."
